21ccabf7eacea54aa218d9b08eabc470c058d02f
sasangachathumal <chathumal.sasanga02@gmail.com>
2021-04-27 08:07:35 +0530

    [CORE-4361] remove tsLint and added esLint

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import { add } from './service/service';
 
-export = function addTwoNumbers(firstNum: any, secondNum: any) {
-  return add(firstNum, secondNum);
-};
+export class PracteraSDK {
+
+  protected environment: string;
+
+  constructor(environment: string) {
+    this.environment = environment;
+  }
+
+  addTwoNumbers(firstNum: any, secondNum: any): number {
+    return add(firstNum, secondNum);
+  }
+
+}
